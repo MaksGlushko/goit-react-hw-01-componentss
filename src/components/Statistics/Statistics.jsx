e0238f4594e374276cdbf75f1584c583e6b16d57
MaksGlushko <maks2007glyshko2@gmail.com>
2023-11-25 13:12:27 +0100

    react 1 hw

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+const Statistics = props => {
+  const { title, stats } = props;
+  return (
+    <section className={css.statistics_tab}>
+      <div className={css.title_thumb}>
+        {title && <h2 className={css.tab_title}>{title.toUpperCase()}</h2>}
+      </div>
+      <ul className={css.tab_statList}>
+        {stats.map(({ id, label, percentage }) => {
+          return (
+            <li key={id} className={css.tab_item}>
+              <span className={css.tab_label}>{label}</span>
+              <span className={css.tab_percentage}>{percentage}%</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string,
+      label: PropTypes.string,
+      percentage: PropTypes.number,
+    })
+  ).isRequired,
+};
+
+export default Statistics;
